2689bcf1e86369c69ca29cc8f6fb022693de1ac7
Hamilton <hamiltonmb.silva@gmail.com>
2020-02-14 09:50:14 -0300

    Configuração da conexão do banco com o sistema, e a criação da classe de Usuarios

---
+++ SistemaControleAluno/Models/ControleContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SistemaControleAluno.Models
+{
+    public class ControleContext : DbContext
+    {
+        public ControleContext() : base("DefaultConnection")
+        {
+                 
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+    }
+}
