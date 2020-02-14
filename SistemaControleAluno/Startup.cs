96d4e6c3be2ba7e5b64db9cce42e32777cef383e
Hamilton <hamiltonmb.silva@gmail.com>
2020-02-14 08:41:56 -0300

    Criação do projeto Sistema de Controle de Aluno

---
+++ SistemaControleAluno/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaControleAluno.Startup))]
+namespace SistemaControleAluno
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
